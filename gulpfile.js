983a40984d1bce7cad3ed73ca60264c86525e6b0
chendesheng <ichendesheng@gmail.com>
2019-12-15 23:27:35 +0800

    add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require("gulp");
+const spawn = require("child_process").spawn;
+
+function log(s) {
+  console.log(`[${new Date()}]: ${s}`);
+}
+
+function exec(cmd) {
+  log(cmd);
+  return new Promise(function(resolve, reject) {
+    const parts = cmd.split(" ");
+    const child = spawn(parts[0], parts.slice(1), {
+      stdio: "inherit",
+    });
+    child.on("close", code => {
+      return code === 0 ? resolve() : reject(code);
+    });
+  });
+}
+
+gulp.task("default", function() {
+  exec("node dev/server");
+  exec("npm run mock");
+  exec("npm run js");
+});
