a909dcd2065605e02a42085f3ff97e763b60f72a
chendesheng <ichendesheng@gmail.com>
2020-01-09 18:08:53 +0800

    simplify genicon

--- dev/genicon.js
+++ dev/genicon.js
@@ -24,6 +24,32 @@ import GroupWorkIcon from "@material-ui/icons/GroupWork";
 import CreateIcon from "@material-ui/icons/Create";
 import CodeIcon from "@material-ui/icons/Code";
 
+const allIcons = {
+  star: StarIcon,
+  dot: DotIcon,
+  send: SendIcon,
+  inbox: InboxIcon,
+  edit: EditIcon,
+  delete: DeleteForeverIcon,
+  view: ViewIcon,
+  add: AddIcon,
+  thumbUp: ThumbUpIcon,
+  thumbDown: ThumbDownIcon,
+  description: DescriptionIcon,
+  image: ImageIcon,
+  viewQuilt: ViewQuiltIcon,
+  settings: SettingsIcon,
+  widgets: WidgetsIcon,
+  dashboard: DashboardIcon,
+  webAsset: WebAssetIcon,
+  chat: ChatIcon,
+  assessment: AssessmentIcon,
+  history: HistoryIcon,
+  groupWork: GroupWorkIcon,
+  create: CreateIcon,
+  code: CodeIcon,
+};
+
 const stripSvgTag = svg => {
   return svg.replace(/\<svg[^>]*?\>|\<\/svg\>/gi, "");
 };
@@ -32,33 +58,8 @@ const getIconContent = icon => {
   return stripSvgTag(ReactDOMServer.renderToString(React.createElement(icon)));
 };
 
-console.error(getIconContent(StarIcon));
-
-const allIcons = {
-  star: getIconContent(StarIcon),
-  dot: getIconContent(DotIcon),
-  send: getIconContent(SendIcon),
-  inbox: getIconContent(InboxIcon),
-  edit: getIconContent(EditIcon),
-  delete: getIconContent(DeleteForeverIcon),
-  view: getIconContent(ViewIcon),
-  add: getIconContent(AddIcon),
-  thumbUp: getIconContent(ThumbUpIcon),
-  thumbDown: getIconContent(ThumbDownIcon),
-  description: getIconContent(DescriptionIcon),
-  image: getIconContent(ImageIcon),
-  viewQuilt: getIconContent(ViewQuiltIcon),
-  settings: getIconContent(SettingsIcon),
-  widgets: getIconContent(WidgetsIcon),
-  dashboard: getIconContent(DashboardIcon),
-  webAsset: getIconContent(WebAssetIcon),
-  chat: getIconContent(ChatIcon),
-  assessment: getIconContent(AssessmentIcon),
-  history: getIconContent(HistoryIcon),
-  groupWork: getIconContent(GroupWorkIcon),
-  create: getIconContent(CreateIcon),
-  code: CodeIcon,
-};
-
 export const genIcons = () =>
-  Object.entries(allIcons).map(([name, svg]) => ({ name, svg }));
+  Object.entries(allIcons).map(([name, icon]) => ({
+    name,
+    svg: getIconContent(icon),
+  }));
