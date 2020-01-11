45e33977b06ee415da13a504130a4441452b3168
AndyLiao8 <zhifu.liao@gmail.com>
2020-01-11 18:06:30 +0800

    add immutable

--- src/components/ImageSelector.tsx
+++ src/components/ImageSelector.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import immutable, { Map } from "immutable";
 import { Theme, makeStyles } from "@material-ui/core/styles";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
@@ -11,7 +12,6 @@ import GridListTileBar from "@material-ui/core/GridListTileBar";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import IconButton from "@material-ui/core/IconButton";
 import Upload, { CFile } from "./Upload";
 import { ifValidFile } from "../framework/utils";
 import { CIcon } from "./Icons";
@@ -49,7 +49,7 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
 }));
 
-let uploadFiles: InternalCImage[] = [];
+let uploadFiles: Map<string, InternalCImage> = Map<string, InternalCImage>({});
 
 const ImageSelector: React.ComponentType<ImageSelectorProps> = ({
   uploadUrl,
@@ -60,19 +60,25 @@ const ImageSelector: React.ComponentType<ImageSelectorProps> = ({
 }) => {
   const classes = useStyles();
   // state
-  const [imageList, setImageList] = useState<InternalCImage[]>([]);
+  const [imageList, setImageList] = useState<Map<string, InternalCImage>>(
+    Map<string, InternalCImage>({}),
+  );
   // effect
   useEffect(() => {
-    uploadFiles = [];
+    uploadFiles = uploadFiles.clear();
   }, []);
   useEffect(() => {
     setImageList(
-      images.map(img => ({
-        ...img,
-        isSelected: false,
-        isUploaded: true,
-        percent: 100,
-      })),
+      images.reduce(
+        (map, img) =>
+          map.set(img.id, {
+            ...img,
+            isSelected: false,
+            isUploaded: true,
+            percent: 100,
+          }),
+        Map<string, InternalCImage>(),
+      ),
     );
   }, []);
   const beforeUpload = (file: CFile) => {
@@ -85,20 +91,17 @@ const ImageSelector: React.ComponentType<ImageSelectorProps> = ({
     }
     const reader = new FileReader();
     reader.onload = () => {
-      const newList = imageList.concat({
-        id: file.uid,
-        name: file.name,
-        url: reader.result as string,
-        isSelected: false,
-        isUploaded: false,
-        percent: 0,
-      });
-      newList.forEach(img => {
-        if (!uploadFiles.some(item => item.id === img.id)) {
-          uploadFiles.push(img);
-        }
-      });
-      setImageList([...uploadFiles]);
+      if (!uploadFiles.has(file.uid)) {
+        uploadFiles = uploadFiles.set(file.uid, {
+          id: file.uid,
+          name: file.name,
+          url: reader.result as string,
+          isSelected: true,
+          isUploaded: false,
+          percent: 0,
+        });
+        setImageList(uploadFiles);
+      }
     };
     reader.readAsDataURL(file);
     return true;
@@ -108,11 +111,14 @@ const ImageSelector: React.ComponentType<ImageSelectorProps> = ({
   const onError = (e: Event, rsp: any, file: CFile) => {};
   const onSelected = () => {
     others.onSelected(
-      imageList.map(o => ({
-        id: o.id,
-        name: o.name,
-        url: o.url,
-      })),
+      imageList
+        .filter(img => !!img?.isSelected)
+        .toArray()
+        .map(o => ({
+          id: o.id,
+          name: o.name,
+          url: o.url,
+        })),
     );
     others.onClose();
   };
@@ -149,15 +155,17 @@ const ImageSelector: React.ComponentType<ImageSelectorProps> = ({
           <Button className={classes.uploadButton}>Upload Image</Button>
         </Upload>
         <GridList className={classes.gridList} cellHeight={160} cols={4}>
-          {imageList.map(img => (
-            <GridListTile key={img.id} cols={1}>
-              <img src={img.url} alt={img.name} />
-              <GridListTileBar
-                titlePosition="top"
-                actionIcon={<CIcon name="checkBox" />}
-              />
-            </GridListTile>
-          ))}
+          {imageList.map(img => {
+            return (
+              <GridListTile key={img?.id} cols={1}>
+                <img src={img?.url} alt={img?.name} />
+                <GridListTileBar
+                  titlePosition="top"
+                  actionIcon={<CIcon name="checkBox" />}
+                />
+              </GridListTile>
+            );
+          })}
         </GridList>
       </DialogContent>
       <DialogActions>
